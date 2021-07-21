0711a3e11093c56801781e21dd2b6ede3e916b74
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-07-20 17:59:52 -0700

    lab07

--- js/app.js
+++ js/app.js
@@ -4,181 +4,45 @@ console.log('hello world');
 
 
 function randomCust(a, b) {
-  let customer = Math.floor(Math.random() * (b - a + 1) + a);
-  return customer;
+  let customerPerHour = Math.floor(Math.random() * (b - a + 1) + a);
+  return customerPerHour;
 }
+
+function Store(location, minHourlyCust, maxHourlyCust, averageCookiePerCust){
+  this.location = location;
+  this.minHourlyCust = minHourlyCust;
+  this.maxHourlyCust = maxHourlyCust;
+  this.averageCookiePerCust = averageCookiePerCust;
+  this.customerPerHour = [];
+  this.cookiePerHour = [];
+  this.results = [];
+}
+
 const hoursArray = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
+const Seattle = new Store('Seattle', 23, 65, 6.3);
+const Tokyo = new Store('Tokyo', 3, 24, 1.2);
+const Dubai = new Store('Dubai', 11, 38, 3.7);
+const Paris = new Store('Paris', 20, 38, 2.3);
+const Lima = new Store('Lima', 2, 16, 4.6);
 
-const seattle = {
-  location: 'seattle',
-  minHourlyCust: 23,
-  maxHourlyCust: 65,
-  averageCookiePerCust: 6.3,
-  customer: 0,
-  cookie: 0,
-  results: 0,
-  getCustPerHour: function () {
-    this.customer = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.customer[index] = randomCust(this.minHourlyCust, this.maxHourlyCust);
-    } return this.customer;
-  },
-  getCookiePerHour: function () {
-    this.cookie = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.cookie[index] = Math.floor(this.getCustPerHour()[index] * this.averageCookiePerCust);
-    } return this.cookie;
-  },
-  getResults: function () {
-    this.results = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.results[index] = hoursArray[index] + ': ' + this.cookie[index] + ' cookies';
-    } return this.results;
-  },
+Store.prototype.getCookiesSold = function(){
+  for (let i=0; i<hoursArray.length; i++){
+    this.customerPerHour[i] = randomCust(this.minHourlyCust, this.maxHourlyCust);
+    this.cookiePerHour[i] = Math.floor(this.customerPerHour[i] * this.averageCookiePerCust);
+    this.results[i] = hoursArray[i] + ': ' + this.cookiePerHour[i] + ' cookies';
+    this.totalCookiesSold += this.cookiePerHour[i];
+  }
 };
 
-
-seattle.getCustPerHour();
-seattle.getCookiePerHour();
-seattle.getResults();
-
-
-
-const tokyo = {
-  location: 'tokyo',
-  minHourlyCust: 3,
-  maxHourlyCust: 24,
-  averageCookiePerCust: 1.2,
-  customer: 0,
-  cookie: 0,
-  results: 0,
-  getCustPerHour: function () {
-    this.customer = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.customer[index] = randomCust(this.minHourlyCust, this.maxHourlyCust);
-    } return this.customer;
-  },
-  getCookiePerHour: function () {
-    this.cookie = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.cookie[index] = Math.floor(this.getCustPerHour()[index] * this.averageCookiePerCust);
-    } return this.cookie;
-  },
-  getResults: function () {
-    this.results = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.results[index] = hoursArray[index] + ': ' + this.cookie[index] + ' cookies';
-    } return this.results;
-  },
-};
-
-tokyo.getCustPerHour();
-tokyo.getCookiePerHour();
-tokyo.getResults();
-
-
-const dubai = {
-  location: 'dubai',
-  minHourlyCust: 11,
-  maxHourlyCust: 38,
-  averageCookiePerCust: 3.7,
-  customer: 0,
-  cookie: 0,
-  results: 0,
-  getCustPerHour: function () {
-    this.customer = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.customer[index] = randomCust(this.minHourlyCust, this.maxHourlyCust);
-    } return this.customer;
-  },
-  getCookiePerHour: function () {
-    this.cookie = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.cookie[index] = Math.floor(this.getCustPerHour()[index] * this.averageCookiePerCust);
-    } return this.cookie;
-  },
-  getResults: function () {
-    this.results = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.results[index] = hoursArray[index] + ': ' + this.cookie[index] + ' cookies';
-    } return this.results;
-  },
-};
-
-dubai.getCustPerHour();
-dubai.getCookiePerHour();
-dubai.getResults();
-
-const paris = {
-  location: 'paris',
-  minHourlyCust: 20,
-  maxHourlyCust: 38,
-  averageCookiePerCust: 2.3,
-  customer: 0,
-  cookie: 0,
-  results: 0,
-  getCustPerHour: function () {
-    this.customer = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.customer[index] = randomCust(this.minHourlyCust, this.maxHourlyCust);
-    } return this.customer;
-  },
-  getCookiePerHour: function () {
-    this.cookie = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.cookie[index] = Math.floor(this.getCustPerHour()[index] * this.averageCookiePerCust);
-    } return this.cookie;
-  },
-  getResults: function () {
-    this.results = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.results[index] = hoursArray[index] + ': ' + this.cookie[index] + ' cookies';
-    } return this.results;
-  },
-};
-
-paris.getCustPerHour();
-paris.getCookiePerHour();
-paris.getResults();
-
-console.log(paris);
-
-const lima = {
-  location: 'lima',
-  minHourlyCust: 2,
-  maxHourlyCust: 16,
-  averageCookiePerCust: 4.6,
-  customer: 0,
-  cookie: 0,
-  results: 0,
-  getCustPerHour: function () {
-    this.customer = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.customer[index] = randomCust(this.minHourlyCust, this.maxHourlyCust);
-    } return this.customer;
-  },
-  getCookiePerHour: function () {
-    this.cookie = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.cookie[index] = Math.floor(this.getCustPerHour()[index] * this.averageCookiePerCust);
-    } return this.cookie;
-  },
-  getResults: function () {
-    this.results = [];
-    for (let index=0; index<hoursArray.length; index++){
-      this.results[index] = hoursArray[index] + ': ' + this.cookie[index] + ' cookies';
-    } return this.results;
-  },
-};
-
-lima.getCustPerHour();
-lima.getCookiePerHour();
-lima.getResults();
-
-const locationArray = [seattle, tokyo, dubai, paris, lima];
-
-const profileDivElem = document.getElementById('cookies');
+function makeElement(tagName, parent, textContent){
+  let element = document.createElement(tagName);
+  if (textContent) {
+    element.textContent = textContent;
+  }
+  parent.appendChild(element);
+  return element;
+}
 
 function renderPlace(place) {
   let articleElem = document.createElement('article');
@@ -195,6 +59,144 @@ function renderPlace(place) {
   }
 }
 
-for (let i = 0; i < locationArray.length; i++){
-  renderPlace(locationArray[i]);
+const locationArray = [Seattle, Tokyo, Dubai, Paris, Lima];
+const profileDivElem = document.getElementById('cookies');
+
+function renderTable(){
+  const articleElem = makeElement('article', profileDivElem, null);
+  const tableElem = makeElement('table', articleElem, null);
+  const theadElem = makeElement('thead', tableElem, null);
+  const rowElem = makeElement('tr', theadElem, null);
+
+  makeElement('th', rowElem, null);
+  for (let i=0; i<hoursArray.length; i++){
+    makeElement('th', rowElem, hoursArray[i]);
+  }
+  makeElement('th', rowElem, 'Daily Location Total');
+
+  const tbodyElem = makeElement('tbody', tableElem, null);
+  for (let i=0; i<locationArray.length; i++){
+    locationArray[i].renderRow(tbodyElem);
+  }
+  const tfootElem = makeElement('tfoot', tableElem, null);
+  const tfRowElem = makeElement('tr', tfootElem, null);
+  makeElement('th', tfRowElem, 'Hourly Total');
+  for(let i=0; i <hoursArray.length; i++){
+    let hourlyTotal = 0;
+    for(let index=0; index<locationArray.length; index++){
+      hourlyTotal += locationArray[index].cookiePerHour[i];
+    }
+    makeElement('th', tfRowElem, hourlyTotal);
+  }
+
 }
+
+Store.prototype.renderRow = function(tbodyElem){
+  const rowElem = makeElement('tr', tbodyElem, null);
+  makeElement('th', rowElem, this.location);
+  let dailyTotal = 0;
+  for (let i=0; i < this.cookiePerHour.length; i++){
+    makeElement('td', rowElem, this.cookiePerHour[i]);
+    dailyTotal += this.cookiePerHour[i];
+  }
+  makeElement('td', rowElem, dailyTotal);
+};
+
+for (let i = 0; i < locationArray.length; i++){
+  locationArray[i].getCookiesSold();
+  renderPlace(locationArray[i]);
+
+}
+
+renderTable();
+
+
+/* <article>
+<table>
+  <thead>
+    <tr>
+      <th> 6 am</th>
+      <th> 7 am</th>
+      <th> 8 am</th>
+      <th> Daily Location total</th>
+    <tr/>
+  </thead>
+  <tbody>
+     <tr>
+      <th> Seattle</th>
+      <td> 200</td>
+      <td> 200</td>
+      <td> 200</td>
+      <td> dailytotalgoeshere</td>
+    <tr/>
+  </tbody>
+  <tfoot>
+    <tr>
+    <th>Totals</th>
+    </tr>
+  </tfoot>
+</table>
+<article> */
+
+//Example of previous way I constructed object. 
+// const seattle = {
+//   location: 'seattle',
+//   minHourlyCust: 23,
+//   maxHourlyCust: 65,
+//   averageCookiePerCust: 6.3,
+//   customerPerHour: [],
+//   cookiePerHour: [],
+//   results: [],
+//   getCustPerHour: function () {
+//     for (let index=0; index<hoursArray.length; index++){
+//       this.customerPerHour[index] = randomCust(this.minHourlyCust, this.maxHourlyCust);
+//     } return this.customerPerHour;
+//   },
+//   getCookiePerHour: function () {
+//     for (let index=0; index<hoursArray.length; index++){
+//       this.cookiePerHour[index] = Math.floor(this.getCustPerHour()[index] * this.averageCookiePerCust);
+//     } return this.cookiePerHour;
+//   },
+//   getResults: function () {
+//     for (let index=0; index<hoursArray.length; index++){
+//       this.results[index] = hoursArray[index] + ': ' + this.cookie[index] + ' cookies';
+//     } return this.results;
+//   },
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+//Example of previous way I constructed object. 
+// const seattle = {
+//   location: 'seattle',
+//   minHourlyCust: 23,
+//   maxHourlyCust: 65,
+//   averageCookiePerCust: 6.3,
+//   customerPerHour: [],
+//   cookiePerHour: [],
+//   results: [],
+//   getCustPerHour: function () {
+//     for (let index=0; index<hoursArray.length; index++){
+//       this.customerPerHour[index] = randomCust(this.minHourlyCust, this.maxHourlyCust);
+//     } return this.customerPerHour;
+//   },
+//   getCookiePerHour: function () {
+//     for (let index=0; index<hoursArray.length; index++){
+//       this.cookiePerHour[index] = Math.floor(this.getCustPerHour()[index] * this.averageCookiePerCust);
+//     } return this.cookiePerHour;
+//   },
+//   getResults: function () {
+//     for (let index=0; index<hoursArray.length; index++){
+//       this.results[index] = hoursArray[index] + ': ' + this.cookie[index] + ' cookies';
+//     } return this.results;
+//   },
+// };
